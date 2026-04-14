8d77d96d2a9151f6e867a632c01a0c413635dfa5
shi.yan <shi.yan@yunzhanghu.com>
2026-04-14 17:33:15 +0800

    添加解约接口

--- api/h5usersign.go
+++ api/h5usersign.go
@@ -14,6 +14,8 @@ type H5UserSignService interface {
 	GetH5UserSignStatus(context.Context, *GetH5UserSignStatusRequest) (*GetH5UserSignStatusResponse, error)
 	// H5UserRelease 用户解约（测试账号专用接口）
 	H5UserRelease(context.Context, *H5UserReleaseRequest) (*H5UserReleaseResponse, error)
+	// H5UserReleaseApply 申请解约
+	H5UserReleaseApply(context.Context, *H5UserReleaseApplyRequest) (*H5UserReleaseApplyResponse, error)
 }
 
 // h5UserSignServiceImpl H5UserSignService 接口实现
@@ -60,6 +62,16 @@ func (c *h5UserSignServiceImpl) GetH5UserSignStatus(ctx context.Context, in *Get
 func (c *h5UserSignServiceImpl) H5UserRelease(ctx context.Context, in *H5UserReleaseRequest) (*H5UserReleaseResponse, error) {
 	out := new(H5UserReleaseResponse)
 	err := c.cc.Invoke(ctx, "POST", "/api/sdk/v1/sign/release", false, in, out)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// H5UserReleaseApply 申请解约
+func (c *h5UserSignServiceImpl) H5UserReleaseApply(ctx context.Context, in *H5UserReleaseApplyRequest) (*H5UserReleaseApplyResponse, error) {
+	out := new(H5UserReleaseApplyResponse)
+	err := c.cc.Invoke(ctx, "POST", "/api/sdk/v1/release/h5", false, in, out)
 	if err != nil {
 		return nil, err
 	}
@@ -164,4 +176,30 @@ type NotifyH5UserSignRequest struct {
 	IDCard string `json:"id_card,omitempty"`
 	// 预签约手机号
 	Phone string `json:"phone,omitempty"`
+}
+
+// H5UserReleaseApplyRequest 申请解约请求
+type H5UserReleaseApplyRequest struct {
+	// 平台企业 ID
+	DealerID string `json:"dealer_id,omitempty"`
+	// 综合服务主体 ID
+	BrokerID string `json:"broker_id,omitempty"`
+	// 姓名
+	RealName string `json:"real_name,omitempty"`
+	// 证件号码
+	IDCard string `json:"id_card,omitempty"`
+	// 主题颜色
+	Color string `json:"color,omitempty"`
+	// 回调 URL
+	URL string `json:"url,omitempty"`
+	// 跳转 URL
+	RedirectURL string `json:"redirect_url,omitempty"`
+}
+
+// H5UserReleaseApplyResponse 申请解约返回
+type H5UserReleaseApplyResponse struct {
+	// 签约状态
+	Status int32 `json:"status,omitempty"`
+	// H5 解约页面 URL
+	URL string `json:"url,omitempty"`
 }
--- api/payment.go
+++ api/payment.go
@@ -284,6 +284,8 @@ type CreateAlipayOrderRequest struct {
 	DealerUserNickname string `json:"dealer_user_nickname,omitempty"`
 	// 用户唯一标识码
 	DealerUserID string `json:"dealer_user_id,omitempty"`
+	// 支付宝转账备注
+	OrderTitle string `json:"order_title,omitempty"`
 }
 
 // CreateAlipayOrderResponse 支付宝实时支付返回
@@ -1073,6 +1075,8 @@ type GetOrderLxlwResponse struct {
 	DeductTax string `json:"deduct_tax,omitempty"`
 	// 追缴个税
 	UserRecoverPersonalTaxAmount string `json:"user_recover_personal_tax_amount,omitempty"`
+	// 支付宝转账备注
+	OrderTitle string `json:"order_title,omitempty"`
 }
 
 // TaxDetail 缴税明细
@@ -1217,6 +1221,8 @@ type NotifyOrderLxlwData struct {
 	DeductTax string `json:"deduct_tax,omitempty"`
 	// 追缴个税
 	UserRecoverPersonalTaxAmount string `json:"user_recover_personal_tax_amount,omitempty"`
+	// 支付宝转账备注
+	OrderTitle string `json:"order_title,omitempty"`
 }
 
 // CancelOrderInBatchRequest 取消批次中单笔订单请求
